61b8cc07563d3ce8286257f5f568847baf2ee862
Soufian Ahlal <ahlal.soufian@alumnat.copernic.cat>
2024-01-23 20:22:02 +0100

    cambios: añadido un card y boton

--- app/src/main/java/com/example/project03/MainActivity.kt
+++ app/src/main/java/com/example/project03/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.project03
 
+import androidx.compose.material3.ElevatedCard
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -7,15 +8,18 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
@@ -27,6 +31,7 @@ import androidx.core.net.toUri
 import com.google.gson.Gson
 import java.io.File
 import java.net.URI
+
 data class Mushroom(
     val commonName: String,
     val scientificName: String,
@@ -41,38 +46,59 @@ val mushrooms = listOf(
     Mushroom("Shiitake", "Lentinula edodes", R.drawable.shiitake_cultivo),
     Mushroom("Porcini", "Boletus edulis", R.drawable._200px_pleurotus_ostreatus_jpg7),
     Mushroom("Oyster Mushroom", "Pleurotus ostreatus", R.drawable.descarga),
-    Mushroom("Reishi", "Ganoderma lucidum", R.drawable.hongo_inmortalidad_anticancerigeno_que_reduce_estres_alarga_vida_98),
+    Mushroom(
+        "Reishi",
+        "Ganoderma lucidum",
+        R.drawable.hongo_inmortalidad_anticancerigeno_que_reduce_estres_alarga_vida_98
+    ),
     Mushroom("Enoki Mushroom", "Flammulina velutipes", R.drawable.istockphoto_505505411_612x612)
 )
 
 @Composable
 fun MushroomList(mushrooms: List<Mushroom>) {
     LazyColumn {
+
         items(mushrooms) { mushroom ->
-            Row(
-                verticalAlignment = Alignment.CenterVertically,
-                modifier = Modifier.padding(16.dp)
-            ) {
-                Image(
-                    painter = painterResource(id = mushroom.imageResourceId),
-                    contentDescription = null,
-                    modifier = Modifier.size(64.dp)
-                )
-                Spacer(modifier = Modifier.width(16.dp))
-                Column {
-                    Text(
-                        text = mushroom.commonName,
-                        fontWeight = FontWeight.Bold,
-                        fontSize = 20.sp,
-                        modifier = Modifier.padding(bottom = 4.dp)
-                    )
-                    Text(
-                        text = mushroom.scientificName,
-                        fontSize = 16.sp,
-                        modifier = Modifier.padding(bottom = 4.dp)
-                    )
+            ElevatedCard {
+                Column(
+                    modifier = Modifier
+                        .padding(vertical = 8.dp)
+                        .fillMaxSize()
+                ) {
+                    Row(
+                        verticalAlignment = Alignment.CenterVertically,
+                        modifier = Modifier.padding(16.dp)
+                    ) {
+                        Image(
+                            painter = painterResource(id = mushroom.imageResourceId),
+                            contentDescription = null,
+                            modifier = Modifier.size(64.dp)
+                        )
+                        Spacer(modifier = Modifier.width(16.dp))
+                        Column {
+                            Text(
+                                text = mushroom.commonName,
+                                fontWeight = FontWeight.Bold,
+                                fontSize = 20.sp,
+                                modifier = Modifier.padding(bottom = 4.dp)
+                            )
+                            Text(
+                                text = mushroom.scientificName,
+                                fontSize = 16.sp,
+                                modifier = Modifier.padding(bottom = 4.dp)
+                            )
+                        }
+                    }
+                    Button(
+                        onClick = {
+                            /* Do something! */
+                        }
+                    ) {
+                        Text("Buy")
+                    }
                 }
             }
+
         }
     }
 }
