762733ad338f153c962860de00c3c0ddbc28dc98
Ainhoa <reyes.moreno.ainhoa.scarlet@alumnat.copernic.cat>
2024-01-23 20:46:56 +0100

    I have completed the navigation menu.

--- app/src/main/java/com/example/project03/MainActivity.kt
+++ app/src/main/java/com/example/project03/MainActivity.kt
@@ -5,10 +5,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.Scaffold
@@ -16,12 +20,14 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.project03.ui.theme.Project03Theme
+import com.example.project03.Items_BottomSheet.*
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
 
 class MainActivity : ComponentActivity() {
@@ -84,16 +90,38 @@ class MainActivity : ComponentActivity() {
     fun ContentBottomSheet(
         mainViewModel: MainViewModel
     ) {
+        val items_menu = listOf(
+            Item_menu01,
+            Item_menu02,
+            Item_menu03
+        )
         Column (
             modifier = Modifier
                 .fillMaxSize()
                 .height(280.dp)
-                .padding(horizontal = 28.dp)
+                .padding(
+                    horizontal = 28.dp,
+
+                    )
         ){
-            Text(
-                text = "Submenu",
-                style = MaterialTheme.typography.bodyMedium
-            )
+            items_menu.forEach{item ->
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    modifier = Modifier
+                        .height(48.dp)
+                ){
+                    Icon(item.icon, item.title)
+                    Spacer(
+                        modifier = Modifier
+                            .width(24.dp)
+                    )
+                    Text(
+                        text = item.title
+                    )
+                }
+                Spacer(modifier = Modifier.height(6.dp))
+            }
+
         }
     }
 
